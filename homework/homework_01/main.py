4b0f4c887e4b2a1849d11a5f50d58691ac29f095
faradg11 <faradg11@gmail.com>
2022-03-18 15:33:47 +0300

    Код функций написан, еще не протестирован

---
+++ homework/homework_01/main.py
@@ -0,0 +1,46 @@
+"""
+Домашнее задание №1
+Функции и структуры данных
+"""
+
+
+def power_numbers(*args):
+    """
+    функция, которая принимает N целых чисел,
+    и возвращает список квадратов этих чисел
+    >>> power_numbers(1, 2, 5, 7)
+    <<< [1, 4, 25, 49]
+    """
+    return [i*i for i in args]
+
+
+# filter types
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
+
+
+def is_prime(num):
+    for i in range(2, round(num ** 0.5) + 1):
+        if num % i == 0:
+            break
+    else:
+        True
+
+def filter_numbers(nums,filter_type):
+    """
+    функция, которая на вход принимает список из целых чисел,
+    и возвращает только чётные/нечётные/простые числа
+    (выбор производится передачей дополнительного аргумента)
+
+    >>> filter_numbers([1, 2, 3], ODD)
+    <<< [1, 3]
+    >>> filter_numbers([2, 3, 4, 5], EVEN)
+    <<< [2, 4]
+    """
+    if filter_type == ODD:
+        return list(filter(lambda x:x % 2 == 1, nums))
+    elif filter_type == EVEN:
+        return list(filter(lambda x:x % 2 == 0, nums))
+    elif filter_type == PRIME:
+        return list(filter(is_prime, nums))
