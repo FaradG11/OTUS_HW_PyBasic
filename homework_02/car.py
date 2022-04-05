a4cd9d9bb7e8149736c75b21e7d87a7d6d702417
faradg11 <faradg11@gmail.com>
2022-04-05 13:41:14 +0300

    car.py is ready

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,15 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+from homework_02.engine import Engine
+
+
+class Car(Vehicle):
+    def __init__(self, weight, fuel, fuel_consumption, engine=None):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.engine = engine
+
+    def set_engine(self, engine):
+        if isinstance(engine, Engine):
+            self.engine = engine
