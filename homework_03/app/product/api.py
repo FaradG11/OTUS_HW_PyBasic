f5ee0cefd69ddcdfdc5fc5d3d866d6f39511bb18
faradg11 <faradg11@gmail.com>
2022-05-15 17:37:35 +0300

    Open amd close sessions using dependency

--- homework_03/app/product/api.py
+++ homework_03/app/product/api.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session as SessionType
 
-from .db_queries import new_income, query_all_product
+from .db_queries import new_income, query_all_product, get_db
 from .schemas import ProductIn, ProductOut, Product
 
 
@@ -8,11 +9,11 @@ router = APIRouter(prefix="/products", tags=['products'])
 
 
 @router.post("", response_model=ProductOut)
-def add_some_product(product_in: ProductIn):
-    return new_income(product_in)
+def add_some_product(product_in: ProductIn, db: SessionType = Depends(get_db)):
+    return new_income(db, product_in)
 
 
 @router.get("", response_model=list[Product])
-def list_of_product():
-    return query_all_product()
+def list_of_product(db: SessionType = Depends(get_db)):
+    return query_all_product(db)
 
--- homework_03/app/product/db_queries.py
+++ homework_03/app/product/db_queries.py
@@ -1,24 +1,28 @@
 from sqlalchemy.orm import Session as SessionType
 
-from .db_setup import Base, Productdb,Session
+from .db_setup import Base, Productdb, Session
 from .schemas import ProductIn, Product
 
+# Dependency for Session closing
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
 
 def create_table():
     Base.metadata.create_all()
     print('Table created')
 
 
-def query_all_product() -> list[Productdb]:
-    session: SessionType = Session()
+def query_all_product(session: SessionType) -> list[Productdb]:
     products = session.query(Productdb).all()
     print("received products:", products)
-    session.close()
     return products
 
 
-def new_income(product_in: ProductIn):
-    session: SessionType = Session()
+def new_income(session: SessionType, product_in: ProductIn):
     product_db = session.query(Productdb).filter_by(name=product_in.name).one_or_none()
     if product_db:
         print('trying to add amount')
