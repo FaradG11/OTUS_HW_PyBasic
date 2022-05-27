9ac89bcbca7510f49cbac24695de9f978f8113b8
faradg11 <faradg11@gmail.com>
2022-05-27 13:09:40 +0300

    Main tasks from homework_4 are complited.

---
+++ homework_04/jsonplaceholder_requests.py
@@ -0,0 +1,20 @@
+
+"""
+создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
+"""
+from typing import List
+
+import aiohttp
+
+
+USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users/"
+POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts/"
+
+
+async def fetch_json(session: aiohttp.ClientSession, url: str) -> List[dict]:
+    async with session.get(url) as response:
+        data: List = await response.json()
+        return data
+
+# async def load_user_data(session: aiohttp.ClientSession, url: str) -> list[dict]:
+
